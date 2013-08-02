0d45b7fa293975a1ba4f705b37c58996aae118fe
Grzegorz Łoś <grzegorz314@gmail.com>
2013-08-02 20:05:38 +0200

    In previous commit also paths to methods tests were changed

---
+++ NBfiles/test/finance/methods/montecarlo/CMCTest.java
@@ -0,0 +1,14 @@
+
+package finance.methods.montecarlo;
+
+import finance.instruments.Instr;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Grzegorz Los
+ */
+public class CMCTest
+{
+}
