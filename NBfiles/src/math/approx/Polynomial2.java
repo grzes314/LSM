a955a88ccb75056a773f4524dbfd028dfaa3e79e
Grzegorz Łoś <grzegorz314@gmail.com>
2013-08-23 21:14:57 +0200

    Class representing two argument polynomials

---
+++ NBfiles/src/math/approx/Polynomial2.java
@@ -0,0 +1,107 @@
+
+package math.approx;
+
+import math.matrices.Vector;
+
+/**
+ *
+ * @author Grzegorz Los
+ */
+public class Polynomial2
+{
+    /**
+     * Constructor which creates double argument polynomial of degree deg from the given
+     * coefficients list. This list represents following numbers:
+     * {#code a[0,0], a[0,1], ..., a[0,deg], a[1,0], a[1,1], ..., a[1,deg-1], ...,
+     * a[deg-1, 0], a[deg-1, 1], a[deg, 0]}. By the {@code x^k y^l} stands {@code a[k,l]}.
+     * Length of array {@code coef} must equal {@code (deg+1)(deg+2)/2}.
+     * @param coef array with coefficients of the polynomial in the form described above.
+     * @param deg degree of the polynomial.
+     * @throws IllegalArgumentException when length of array {@code 
+     */
+    public Polynomial2(double[] coef, int deg)
+    {
+        ensureCoefHasProperLength(coef, deg);
+        this.deg = deg;
+        a = new double[deg+1][];
+        for (int i = 0; i <= deg; ++i)
+        {
+            a[i] = new double[deg - i + 1];
+            for (int j = 0; j <= deg - i; ++j)
+                a[i][j] = coef[i+j];
+        }
+    }
+    
+    /**
+     * Constructor which work similarly to the above one, but is gives coefficients
+     * in the Vector instead in the array.
+     * @param coefs
+     * @param deg 
+     */
+    public Polynomial2(Vector coef, int deg)
+    {
+        ensureCoefHasProperLength(coef, deg);
+        this.deg = deg;
+        a = new double[deg+1][];
+        for (int i = 0; i <= deg; ++i)
+        {
+            a[i] = new double[deg - i + 1];
+            for (int j = 0; j <= deg - i; ++j)
+                a[i][j] = coef.get(i+j+1);
+        }
+    }
+    
+    /**
+     * Creates constant polynomial.
+     * @param constant value of the polynomial in each point of the domain.
+     */
+    public Polynomial2(double constant)
+    {
+        this.deg = 0;
+        a = new double[1][1];
+        a[0][0] = constant;
+    }
+
+    public int getDeg()
+    {
+        return deg;
+    }
+    
+    public double value(double x, double y)
+    {
+        double x_pow = 1;
+        double sum = 0;
+        for (int i = 0; i <= deg; ++i)
+        {
+            sum += valueOfSubPolynomial(i, x_pow, y);
+            x_pow *= x;
+        }
+        return sum;
+    }
+    
+    private double valueOfSubPolynomial(int xDeg, double x_pow, double y)
+    {
+        double subsum = 0;
+        for (int j = deg - xDeg; j >= 0; --j)
+            subsum = subsum*y + a[xDeg][j];
+        return x_pow * subsum;
+    }
+    
+    private void ensureCoefHasProperLength(double[] coef, int deg)
+    {
+        int l = (deg+1) * (deg+2) / 2;
+        if (l != coef.length)
+            throw new IllegalArgumentException("Coefficients array should have length " + l);
+    }    
+    
+    private void ensureCoefHasProperLength(Vector coef, int deg)
+    {
+        int l = (deg+1) * (deg+2) / 2;
+        if (l != coef.getSize())
+            throw new IllegalArgumentException("Coefficients array should have length " + l);
+    }
+    
+    private double[][] a;
+    
+    private int deg;
+}
---
+++ NBfiles/test/math/approx/Polynomial2Test.java
@@ -0,0 +1,76 @@
+
+package math.approx;
+
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Grzegorz Los
+ */
+public class Polynomial2Test extends TestCase
+{
+    @Override
+    public void setUp()
+    {
+        constant = new Polynomial2(5.0);
+        makeAllOnes();
+        makeSmallRandom();
+    }
+    
+    private void makeAllOnes()
+    {
+        int n = 50;
+        int m = (n+1) * (n+2) / 2;
+        double[] a = new double[m];
+        for (int i = 0; i < m; ++i)
+            a[i] = 1;
+        allOnes = new Polynomial2(a, n);
+    }
+    
+    private void makeSmallRandom()
+    {
+        int n = 2;
+        smallRandomCoefs          //y^0 y^1 y^2
+                   = new double[] { -1,  2, -1,  // x^0
+                                    -3,  2,      // x^1
+                                    -2 };        // x^2
+        smallRandom = new Polynomial2(smallRandomCoefs, n);
+    }
+    
+    public void testConstPolynomial()
+    {
+        assertEquals(5.0, constant.value(0, 0), delta);
+        assertEquals(5.0, constant.value(1, 0), delta);
+        assertEquals(5.0, constant.value(0, 1), delta);
+        assertEquals(5.0, constant.value(1, 1), delta);
+        assertEquals(5.0, constant.value(Math.PI, 2.34), delta);
+        assertEquals(5.0, constant.value(3434324, 2131231), delta);
+    }
+    
+    public void testAllOnes()
+    {
+        assertEquals(1.0, allOnes.value(0, 0), delta);
+        double x = 0.9, y = 1.1;
+        int n = allOnes.getDeg();
+        double sum = 0;
+        for (int i = 0; i <= n; ++i)
+            for (int j = 0; j <= n-i; ++j)
+                sum += Math.pow(x, i) * Math.pow(y, j);
+        assertEquals(sum, allOnes.value(x, y), delta);
+    }
+    
+    public void testSmallRandom()
+    {
+        double sum = 0;
+        int n = smallRandom.getDeg();
+        double x = 2.5, y = -3.3;
+        for (int i = 0; i <= n; ++i)
+            for (int j = 0; j <= n-i; ++j)
+                sum += smallRandomCoefs[i+j] * Math.pow(x, i) * Math.pow(y, j);
+        assertEquals(sum, smallRandom.value(x, y), delta);
+    }
+    
+    private double delta = 0.0001;
+    private Polynomial2 constant, allOnes, smallRandom;
+    private double[] smallRandomCoefs;
+}
