eec27b379c1b515c1cb9a46647d6bcd62ad9ac19
Grzegorz Łoś <grzegorz314@gmail.com>
2013-09-01 14:11:58 +0200

    Further corrections in Preliminaries

--- R/rnormTest.R
+++ R/rnormTest.R
@@ -36,38 +36,38 @@ testNormalGenerators <- function(n)
 ################################################################################
 # inne podejscie
 
-boxMuller <- function(n)
-{
-  U1 <- runif(n)
-  U2 <- runif(n)
-  roots <- sqrt( -2 * log(U1) )
-  N1 <- roots * cos(2 * pi * U2)
-  N2 <- roots * sin(2 * pi * U2)
-  c(N1, N2)
-}
-
-polarRejection <- function(n)
-{
-  V1 <- 2*runif(1.5*n) - 1
-  V2 <- 2*runif(1.5*n) - 1
-  W <- V1^2 + V2^2
-  hit <- which(W <= 1)
-  V1 <- (V1[hit])[1:n]
-  V2 <- (V2[hit])[1:n]
-  W <- (W[hit])[1:n]
-  root <- sqrt( -2 * log(W) / W )
-  N1 <- root * V1
-  N2 <- root * V2
-  c(N1, N2)
-}
-
-testNormalGenerators <- function(n)
-{
-  bm <- system.time( boxMuller(n) )
-  pr <- system.time( polarRejection(n) )
-  print(paste("Box-Muller:", round(bm[1], 2), "seconds."))
-  print(paste("Polar rejection method:", round(pr[1], 2), "seconds."))
-}
+# boxMuller <- function(n)
+# {
+#   U1 <- runif(n)
+#   U2 <- runif(n)
+#   roots <- sqrt( -2 * log(U1) )
+#   N1 <- roots * cos(2 * pi * U2)
+#   N2 <- roots * sin(2 * pi * U2)
+#   c(N1, N2)
+# }
+# 
+# polarRejection <- function(n)
+# {
+#   V1 <- 2*runif(1.5*n) - 1
+#   V2 <- 2*runif(1.5*n) - 1
+#   W <- V1^2 + V2^2
+#   hit <- which(W <= 1)
+#   V1 <- (V1[hit])[1:n]
+#   V2 <- (V2[hit])[1:n]
+#   W <- (W[hit])[1:n]
+#   root <- sqrt( -2 * log(W) / W )
+#   N1 <- root * V1
+#   N2 <- root * V2
+#   c(N1, N2)
+# }
+# 
+# testNormalGenerators <- function(n)
+# {
+#   bm <- system.time( boxMuller(n) )
+#   pr <- system.time( polarRejection(n) )
+#   print(paste("Box-Muller:", round(bm[1], 2), "seconds."))
+#   print(paste("Polar rejection method:", round(pr[1], 2), "seconds."))
+# }
 
 ################################################################################
 ################## rysunek zmiennych skorelowanych #############################
@@ -81,36 +81,30 @@ randomCorr <- function(corr)
   )
 }
 
+
+makeOneScatterPlot <- function(corr)
+{
+  pairs <- data.frame(t(sapply(1:1000, function(foo) randomCorr(corr))))
+  names(pairs) <- c("Z1", "Z2")
+  qplot(x=Z1, y=Z2, data=pairs) +
+    theme_bw(base_size=16) + geom_point()
+}
+
 draw <- function()
 {
   require(grid)
   
   vp1 <- viewport(width=0.5, height=0.5, x=0.25, y=0.75)
-  pairs <- data.frame(t(sapply(1:1000, function(foo) randomCorr(-0.8))))
-  names(pairs) <- c("Z1", "Z2")
-  wyk1 <- qplot(x=Z1, y=Z2, data=pairs) +
-    geom_point()
-  
+  wyk1 <- makeOneScatterPlot(-0.9)
   
   vp2 <- viewport(width=0.5, height=0.5, x=0.75, y=0.75)
-  pairs <- data.frame(t(sapply(1:1000, function(foo) randomCorr(-0.4))))
-  names(pairs) <- c("Z1", "Z2")
-  wyk2 <- qplot(x=Z1, y=Z2, data=pairs) +
-    geom_point()
-  
+  wyk2 <- makeOneScatterPlot(-0.5)
   
   vp3 <- viewport(width=0.5, height=0.5, x=0.25, y=0.25)
-  pairs <- data.frame(t(sapply(1:1000, function(foo) randomCorr(0))))
-  names(pairs) <- c("Z1", "Z2")
-  wyk3 <- qplot(x=Z1, y=Z2, data=pairs) +
-    geom_point()
-  
+  wyk3 <- makeOneScatterPlot(0)
   
   vp4 <- viewport(width=0.5, height=0.5, x=0.75, y=0.25)
-  pairs <- data.frame(t(sapply(1:1000, function(foo) randomCorr(0.4))))
-  names(pairs) <- c("Z1", "Z2")
-  wyk4 <- qplot(x=Z1, y=Z2, data=pairs) +
-    geom_point()
+  wyk4 <- makeOneScatterPlot(0.5)
   
   vp <- viewport()
   print(wyk1, vp=vp1)
